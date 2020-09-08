65a5f97e2db33f571da51ecd915e959d3ff5aa4e
Onju <onjujpn@gmail.com>
2020-09-09 02:07:40 +0900

    indexeddb base

--- hoge.js
+++ hoge.js
@@ -1,12 +1,49 @@
-$("#btn1").on("click", function()  {
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    let message = `今日は${year}年${month}月${day}日です`
-    // jQueryを使って画面にメッセージを表示する
-    $("#tBox").val(message);
+const dbName = "ontestDB";
 
+$("#btnExe").on("click", function () {
+  DbOpen(function (db) {
+    console.log("version:" + db.version);
 
+    let customerObjectStoreReq = db.transaction('testobj', 'readonly').objectStore('testobj');
 
+    //$("#tbox").val("");
   });
+});
+
+$("#btnDelete").on("click", function () {
+  DbDelete();
+});
+
+
+function DbOpen(callback ) {
+  let openReq = indexedDB.open(dbName);
+
+  openReq.onerror = function (event) {
+    console.log("db open error");
+  };
+
+  openReq.onupgradeneeded = function (event) {
+    console.log("db upgraded");
+  };
+
+  openReq.onsuccess = function (event) {
+    console.log("db opened");
+    let db = event.target.result;
+
+    callback(db);
+
+    db.close();
+  };
+}
+
+function DbDelete() {
+  deleteReq = indexedDB.deleteDatabase(dbName);
+
+  deleteReq.onsuccess = function (event) {
+    console.log("db deleted");
+  };
+
+  deleteReq.onerror = function () {
+    console.log("db delete error");
+  };
+}
